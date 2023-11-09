7c64fa670cc1615d9febfb119a12502160f8216b
Alan Oakden <alanoakden@alans-mbp.home>
2023-11-09 18:59:19 +0000

    Escaped script for ad words.

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -8,11 +8,13 @@ export default function App({ Component, pageProps }) {
     <Component {...pageProps} />
     <Script async src="https://www.googletagmanager.com/gtag/js?id=AW-11405710909" />
     <Script>
+      {  `      
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments)}
         gtag('js', new Date());
   
-        gtag('config', 'AW-11405710909');
+        gtag('config', 'AW-11405710909');`}
+
     </Script>
     </>
   )
