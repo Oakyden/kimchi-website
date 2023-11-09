07e819196c5651845e9cce2fc73c2b6225c10159
Alan Oakden <alanoakden@alans-mbp.home>
2023-11-09 18:43:34 +0000

    Attempted to add ad words script via next script component.

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,6 +1,20 @@
 import 'focus-visible'
 import '@/styles/tailwind.css'
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+    <Component {...pageProps} />
+    <Script async src="https://www.googletagmanager.com/gtag/js?id=AW-11405710909" />
+    <Script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments)}
+        gtag('js', new Date());
+  
+        gtag('config', 'AW-11405710909');
+    </Script>
+    </>
+  )
+
 }
